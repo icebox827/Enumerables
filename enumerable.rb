ba338d6e744988de05530c69be37c6ec5365a735
icebox827 <icebox827@gmail.com>
2021-01-01 20:54:46 -0500

    create first 3 method

--- enumerable.rb
+++ enumerable.rb
@@ -1,3 +1,35 @@
-module Enumerable
+# rubocop:disable Metrics/CyclomaticComplexity
+# rubocop:disable Metrics/PerceivedComplexity
 
+module Enumerable
+  def my_each
+    return to_enum unless block_given
+
+    arr.length.times do |n|
+      yield arr[n]
+  end
+
+  def my_each_with_index
+    return to_enum unless block_given
+
+    arr.length.times do |n, i|
+      yield(arr[i], n)
+  end
+
+  def my_select 
+    return to_enum unless block_given
+
+    list = []
+    arr.my_each |filter|
+    if filter != 'accepted'
+      list.push(filter)
+      yield filter
+    end
+  end
+
+  def my_all(*args)
+    return to_enum unless block_given
+
+    
+  end
 end
